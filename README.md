d8f16f0803e4a4b7f589a2f4bbe963cb7f235eea
Richard Kuhnt <r15ch13+git@gmail.com>
2019-04-23 17:42:40 +0200

    First release

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scoop Clone
+
+A small script that clones a Git repository to a target directory without requiring a complete installation of Git.
+
+This is a helper script for [Scoop](https://scoop.sh), the Windows command-line installer.
+
+# Usage
+
+```pwsh
+.\scoop-clone.ps1 -Repository 'https://github.com/<user>/<repo>/' -Directory '.\test-repo'
+```
---
+++ build.ps1
@@ -0,0 +1,38 @@
+Push-Location $PSScriptRoot
+
+Get-ChildItem env:
+
+# Prepare
+$build = "$PSScriptRoot\build"
+$dist = "$PSScriptRoot\dist"
+New-Item -ItemType Directory -Path $build -ErrorAction SilentlyContinue | Out-Null
+New-Item -ItemType Directory -Path $dist -ErrorAction SilentlyContinue | Out-Null
+Remove-Item "$build\*" -Recurse -Force | Out-Null
+Remove-Item "$dist\*" -Recurse -Force | Out-Null
+
+# Build
+Copy-Item "$PSScriptRoot\scoop-clone.ps1" $build
+Copy-Item "$PSScriptRoot\packages\LibGit2Sharp\lib\net46\LibGit2Sharp.dll" $build
+New-Item -ItemType Directory -Path "$build\lib\win32\x64\" -ErrorAction SilentlyContinue | Out-Null
+Copy-Item "$PSScriptRoot\packages\LibGit2Sharp.NativeBinaries\runtimes\win-x64\native\git2-*.dll" "$build\lib\win32\x64\"
+New-Item -ItemType Directory -Path "$build\lib\win32\x86\" -ErrorAction SilentlyContinue | Out-Null
+Copy-Item "$PSScriptRoot\packages\LibGit2Sharp.NativeBinaries\runtimes\win-x86\native\git2-*.dll" "$build\lib\win32\x86\"
+
+# Checksums
+Write-Output 'Computing checksums ...'
+Get-ChildItem "$build\*" -Include *.ps1,*.dll -Recurse | ForEach-Object {
+    $checksum = (Get-FileHash -Path $_.FullName -Algorithm SHA256).Hash.ToLower()
+    $line = "$checksum *$($_.FullName.Replace($build, '').TrimStart('\'))"
+    Write-Output $line
+    $line | Out-File "$build\checksums.sha256" -Append -Encoding oem
+}
+
+# Package
+7z a "$dist\scoop-clone.zip" "$build\*"
+Get-ChildItem "$dist\*" | ForEach-Object {
+    $checksum = (Get-FileHash -Path $_.FullName -Algorithm SHA256).Hash.ToLower()
+    $line = "$checksum *$($_.Name)"
+    Write-Output $line
+    $line | Out-File "$dist\$($_.Name).sha256" -Append -Encoding oem
+}
+Pop-Location
